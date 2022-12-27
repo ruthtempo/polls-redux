a9271573b0176576e443f53f177068fc49dcae99
Ruth Temporal <ruthtemp@gmail.com>
2022-12-27 15:00:59 +0100

    adds button type

--- src/ui/Button.js
+++ src/ui/Button.js
@@ -1,6 +1,9 @@
 export const Button = ({ buttonText }) => {
   return (
-    <button className="border-solid bg-indigo-500 p-3 rounded-full text-white capitalize">
+    <button
+      className="border-solid bg-indigo-500 hover:bg-indigo-700 py-2 my-2 rounded-full text-white capitalize cursor-pointer"
+      type="button"
+    >
       {buttonText}
     </button>
   );
