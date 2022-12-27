ab204dc7d13317a8fd6f6943a896fa0d9d3b0f01
Ruth Temporal <ruthtemp@gmail.com>
2022-12-27 19:54:27 +0100

    styles navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
 import { Link } from "react-router-dom";
+import { Button } from "../ui/Button.js";
 
 export const Navbar = () => {
   return (
-    <nav>
-      <Link to="home">Dashboard</Link>
-      <Link to="new poll">New Poll</Link>
-      <Link to="leaderboard">LeaderBoard</Link>
+    <nav className="w-full bg-indigo-300 p-4 flex justify-between">
+      <div className="flex items-center justify-items-center">
+        <Link to="home" className="p-2 uppercase">
+          Dashboard
+        </Link>
+        <Link to="new poll" className="p-2 uppercase">
+          New Poll
+        </Link>
+        <Link to="leaderboard" className="p-2 uppercase">
+          LeaderBoard
+        </Link>
+      </div>
+      <div className="flex flex-col">
+        user +avatar
+        <Button buttonText={"Log out"} />
+      </div>
     </nav>
   );
 };
