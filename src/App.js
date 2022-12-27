3762fc6768510e330f896b7d82db22ab5aa3b1f8
Ruth Temporal <ruthtemp@gmail.com>
2022-12-27 15:02:02 +0100

    Login to LoginForm

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
+import LoginForm from "./components/Login";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
 
 export default function App() {
   return (
-    <div className="container">
+    <div className="container w-full p-2">
       <Navbar />
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LoginForm />} />
       </Routes>
     </div>
   );
