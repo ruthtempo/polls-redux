0a748602183f5d122c30074a9459209f106dcc45
Ruth Temporal <ruthtemp@gmail.com>
2022-12-27 13:53:41 +0100

    creates reusable button component

---
+++ src/ui/Button.js
@@ -0,0 +1,7 @@
+export const Button = ({ buttonText }) => {
+  return (
+    <button className="border-solid bg-indigo-500 p-3 rounded-full text-white capitalize">
+      {buttonText}
+    </button>
+  );
+};
