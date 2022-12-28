6723c9d9594b58f283899aafd0dcdec88b00ffdf
Ruth Temporal <ruthtemp@gmail.com>
2022-12-28 14:29:41 +0100

    adds props to custom button

--- src/ui/Button.js
+++ src/ui/Button.js
@@ -1,9 +1,12 @@
-export const Button = ({ buttonText, disabled }) => {
+export const Button = ({ buttonText, disabled, onClick, className }) => {
   return (
     <button
-      className="drop-shadow-md border-solid bg-indigo-500 hover:bg-indigo-700 py-2 my-2 rounded-full text-white capitalize disabled:bg-slate-50 disabled:text-slate-500  disabled:border-slate-200 disabled:shadow-none"
+      className={`drop-shadow-md border-solid bg-indigo-500 hover:bg-indigo-700 py-2 px-3 my-2 rounded-full text-white capitalize disabled:bg-slate-50 disabled:text-slate-500  disabled:border-slate-200 disabled:shadow-none ${
+        className ?? ""
+      }`}
       type="button"
       disabled={disabled}
+      onClick={onClick}
     >
       {buttonText}
     </button>
