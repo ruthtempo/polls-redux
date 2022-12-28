9c2227564a533fb80627abec0321fff871d55eed
Ruth Temporal <ruthtemp@gmail.com>
2022-12-28 14:32:10 +0100

    login form binded to store

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,58 @@
 import { useState } from "react";
+import setAuthedUser from "../actions/authedUser";
 import { Button } from "../ui/Button";
 import { DropdownButton } from "../ui/DropdownButton";
+import { DropdownItem } from "../ui/DropdownItem";
+import { connect } from "react-redux";
 
-const LoginForm = () => {
+const LoginForm = ({ dispatch, users, loading }) => {
+  const [selectedUser, setSelectedUser] = useState();
+  const [password, setPassword] = useState();
+
+  const handleChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (password === selectedUser.password) {
+      dispatch(setAuthedUser(selectedUser));
+    } else {
+      console.log("wrong password");
+    }
+  };
+
   return (
-    <div className="rounded bg-blue-100 p-2 m-2 flex justify-center">
-      <form className="flex flex-col w-2/5">
-        <DropdownButton buttonText={"choose user"} />
-        <label className="mt-3">Password</label>
-        <input className="rounded-md p-1 my-2" />
-        <Button buttonText={"Log in"} />
-      </form>
+    <div className="rounded  p-2 m-2 flex flex-col items-center h-5/6">
+      <h1 className="text-3xl my-2">Employee Polls</h1>
+      <div className="flex flex-col w-2/5">
+        <DropdownButton buttonText={selectedUser?.id ?? "choose user"}>
+          {loading
+            ? null
+            : Object.values(users).map((user) => (
+                <DropdownItem
+                  key={user.id}
+                  onClick={() => setSelectedUser(user)}
+                >
+                  {user.id}
+                </DropdownItem>
+              ))}
+        </DropdownButton>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          className="rounded-md p-2 my-2"
+          onChange={handleChange}
+        />
+        <Button
+          buttonText={"Log in"}
+          onClick={handleSubmit}
+          disabled={!selectedUser || !password}
+        />
+      </div>
     </div>
   );
 };
 
-export default LoginForm;
+const mapStateToProps = ({ users }) => ({ loading: users === null, users });
+
+export default connect(mapStateToProps)(LoginForm);
