2337a9dff4c43420ff7f40c7ea935916b352d2f0
Ruth Temporal <ruthtemp@gmail.com>
2022-12-28 14:29:19 +0100

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import LoginForm from "./components/Login";
 import { Home } from "./components/Home";
-import { Navbar } from "./components/Navbar";
+import Navbar from "./components/Navbar";
+import { handleInitialData } from "./actions/users";
+import { connect } from "react-redux";
 
-export default function App() {
+function App(props) {
+  useEffect(() => {
+    props.dispatch(handleInitialData());
+  }, [props]);
+
   return (
     <div className="h-screen w-full bg-indigo-100">
-      <Navbar />
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<LoginForm />} />
-      </Routes>
+      {/* if autheduser show navbar+routes, else show loginform */}
+      {props.authedUser !== null ? (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </>
+      ) : (
+        <LoginForm />
+      )}
     </div>
   );
 }
+const mapStateToProps = ({ authedUser }) => ({
+  // access state props (input state)
+  authedUser, // return state prop (output props)
+});
+export default connect(mapStateToProps)(App);
