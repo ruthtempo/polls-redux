ba8040b21446628edffea98a04de3a12a22a1c73
Ruth Temporal <ruthtemp@gmail.com>
2022-12-29 20:46:33 +0100

    button component added styles

--- src/ui/Button.js
+++ src/ui/Button.js
@@ -1,7 +1,7 @@
 export const Button = ({ buttonText, disabled, onClick, className }) => {
   return (
     <button
-      className={`drop-shadow-md border-solid bg-indigo-500 hover:bg-indigo-700 py-2 px-3 my-2 rounded-full text-white capitalize disabled:bg-slate-50 disabled:text-slate-500  disabled:border-slate-200 disabled:shadow-none ${
+      className={`drop-shadow-md border-solid bg-indigo-500 hover:bg-indigo-700 py-2 px-3 my-2 rounded-full text-white text-center capitalize disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none ${
         className ?? ""
       }`}
       type="button"
