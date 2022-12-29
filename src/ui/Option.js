eaa2c1e5058cffc638433e2150b42ee7c01fe733
Ruth Temporal <ruthtemp@gmail.com>
2022-12-29 20:45:35 +0100

    created custom component option

---
+++ src/ui/Option.js
@@ -0,0 +1,12 @@
+export const Option = ({ children, className, onClick }) => {
+  return (
+    <div
+      className={` text-center max-w-md bg-indigo-300 hover:bg-teal-300 p-4 text-xl drop-shadow-md cursor-pointer m-2 basis-1/2 ${
+        className ?? ""
+      }`}
+      onClick={onClick}
+    >
+      {children}
+    </div>
+  );
+};
