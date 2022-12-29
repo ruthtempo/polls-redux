daf9eb0dee22997fecbc35acbb415834f17112e5
Ruth Temporal <ruthtemp@gmail.com>
2022-12-29 20:46:19 +0100

    custom component Navlink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,24 @@
 import { connect } from "react-redux";
-import { NavLink } from "react-router-dom";
 import setAuthedUser from "../actions/authedUser.js";
 import { Button } from "../ui/Button.js";
+import { NavLink } from "../ui/NavLink.js";
 
 const Navbar = (props) => {
   return (
     <nav className="w-full bg-indigo-200 p-4 flex justify-between">
       <div className="flex items-center justify-items-center">
-        <NavLink
-          to="/"
-          className={`pr-2 ${({ isActive }) =>
-            isActive ? "text-indigo-600 uppercase" : ""}`}
-        >
-          Dashboard
-        </NavLink>
-        <NavLink to="new poll">New Poll</NavLink>
-        <NavLink to="leaderboard">LeaderBoard</NavLink>
+        <NavLink to="/">Dashboard</NavLink>
+        <NavLink to="new-poll">New Poll</NavLink>
+        <NavLink to="leaderboard">Leaderboard</NavLink>
       </div>
       <div className="flex flex-col">
         <div className="flex items-center">
-          <div className="rounded-full bg-gray-200 mr-3 w-20">
-            <img
-              src={props.authedUser.avatarURL}
-              alt="user-avatar"
-              className="object-contain rounded-full"
-            />
-          </div>
-          <p className="text-lg mr-3">{props.authedUser.id}</p>
+          <img
+            src={props.authedUser.avatarURL}
+            alt="user-avatar"
+            className="object-contain rounded-full mr-3 w-20"
+          />
+          2<p className="text-lg mr-3">{props.authedUser.id}</p>
         </div>
 
         <Button
---
+++ src/ui/NavLink.js
@@ -0,0 +1,14 @@
+import { NavLink as RouterLink } from "react-router-dom";
+
+export const NavLink = ({ to, children }) => {
+  return (
+    <RouterLink
+      to={to}
+      className={({ isActive }) =>
+        `${isActive ? "text-indigo-600  font-bold" : ""} pr-2 tracking-wide`
+      }
+    >
+      {children}
+    </RouterLink>
+  );
+};
