1835caf8cf6315dddd152aee1c51dd273423da6c
Ruth Temporal <ruthtemp@gmail.com>
2022-12-30 20:16:08 +0100

    styles

--- src/ui/Option.js
+++ src/ui/Option.js
@@ -1,7 +1,7 @@
 export const Option = ({ children, className, onClick, disabled }) => {
   return (
     <button
-      className={`bg-indigo-300 hover:bg-teal-300 disabled:opacity-75 disabled:pointer-events-none text-center max-w-md p-4 text-xl drop-shadow-md cursor-pointer m-2 ${
+      className={` w-full bg-indigo-300 hover:bg-teal-300 disabled:opacity-75 disabled:pointer-events-none text-center max-w-md p-4 text-xl drop-shadow-md cursor-pointer m-2 ${
         className ?? ""
       }`}
       onClick={onClick}
