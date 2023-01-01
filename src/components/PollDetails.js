99256b7dde2227b76df55266cc6104050484192d
Ruth Temporal <ruthtemp@gmail.com>
2023-01-01 13:52:07 +0100

    option component added props

--- src/components/PollDetails.js
+++ src/components/PollDetails.js
@@ -1,19 +1,15 @@
-import { useState } from "react";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 import { handleSaveAnswer } from "../actions/questions";
 import { Option } from "../ui/Option";
+import NotFound from "./NotFound";
 
 const PollDetails = ({ questions, users, loading, authedUser, dispatch }) => {
   const { question_id } = useParams();
 
-  const question =
-    questions && Object.values(questions).filter((q) => q.id === question_id); //to get the question matching the id received by router param
+  const question = questions?.[question_id]; //to get the question matching the id received by router param
 
-  const user =
-    users &&
-    question &&
-    Object.values(users).filter((user) => user.id === question[0].author); // to get the avatar that matches the poll author
+  const author = users?.[question?.author]; // to get the avatar that matches the poll author
 
   const hasChosenAnswer = authedUser && authedUser.answers[question_id] != null; //chech if id is in answers object to disable both buttons once poll is answered
 
@@ -23,71 +19,55 @@ const PollDetails = ({ questions, users, loading, authedUser, dispatch }) => {
 
   const percentageOption2 =
     questions &&
+    question &&
     Math.round(
-      (question[0].optionTwo.votes.length * 100) / Object.keys(questions).length
+      (question.optionTwo.votes.length * 100) / Object.keys(questions).length
     );
   const percentageOption1 =
     questions &&
+    question &&
     Math.round(
-      (question[0].optionOne.votes.length * 100) / Object.keys(questions).length
+      (question.optionOne.votes.length * 100) / Object.keys(questions).length
     );
 
-  return (
+  return questions?.[question_id] == null ? (
+    <NotFound />
+  ) : (
     question && (
-      <div className="bg-indigo-100 py-4">
+      <div className="bg-indigo-100 py-4 grid grid-cols-1 justify-items-center">
         {loading ? null : (
           <>
             <div className="flex flex-col items-center">
               <h4 className="text-2xl mb-4 text-gray-800">
-                Poll by {question[0].author}
+                Poll by {question.author}
               </h4>
               <img
-                src={user && user[0].avatarURL}
+                src={author?.avatarURL}
                 alt="user-avatar"
                 className="rounded-full w-32 border-solid border-4 border-teal-200"
               />
               <h4 className="text-4xl my-4">Would you rather</h4>
             </div>
 
-            <div className="flex flex-wrap justify-center">
-              <div className="mr-2">
-                <Option
-                  className={`rounded-l-lg w-full ${
-                    authedUser.answers[question_id] === "optionOne"
-                      ? "bg-teal-300"
-                      : ""
-                  }`}
-                  disabled={hasChosenAnswer}
-                  onClick={handleAnswerClick}
-                >
-                  {question[0].optionOne.text}
-                </Option>
-                {hasChosenAnswer && (
-                  <p className="text-center text-gray-600">
-                    {question[0].optionOne.votes.length} votes (
-                    {percentageOption1} %)
-                  </p>
-                )}
-              </div>
-              <div className="">
-                <Option
-                  className={`rounded-r-lg w-full ${
-                    authedUser.answers[question_id] === "optionTwo"
-                      ? "bg-teal-300"
-                      : ""
-                  }`}
-                  disabled={hasChosenAnswer}
-                  onClick={handleAnswerClick}
-                >
-                  {question[0].optionTwo.text}
-                </Option>
-                {hasChosenAnswer && (
-                  <p className="text-center text-gray-600">
-                    {question[0].optionTwo.votes.length} votes (
-                    {percentageOption2}%)
-                  </p>
-                )}
-              </div>
+            <div className="grid grid-cols-1 md:grid-cols-2">
+              <Option
+                className="rounded-l-lg"
+                isSelected={authedUser.answers[question_id] === "optionOne"}
+                disabled={hasChosenAnswer}
+                onClick={handleAnswerClick}
+                percentage={percentageOption1}
+                votesLength={question.optionOne.votes.length}
+                text={question.optionOne.text}
+              />
+              <Option
+                className="rounded-r-lg"
+                isSelected={authedUser.answers[question_id] === "optionTwo"}
+                disabled={hasChosenAnswer}
+                onClick={handleAnswerClick}
+                percentage={percentageOption2}
+                votesLength={question.optionTwo.votes.length}
+                text={question.optionTwo.text}
+              />
             </div>
             {hasChosenAnswer && (
               <p className="text-center italic mt-4 text-gray-500">
--- src/ui/Option.js
+++ src/ui/Option.js
@@ -1,13 +1,40 @@
-export const Option = ({ children, className, onClick, disabled }) => {
+export const Option = ({
+  className,
+  onClick,
+  disabled,
+  votesLength,
+  percentage,
+  isSelected,
+  text,
+}) => {
   return (
-    <button
-      className={` w-full bg-indigo-300 hover:bg-teal-300 disabled:opacity-75 disabled:pointer-events-none text-center max-w-md p-4 text-xl drop-shadow-md cursor-pointer m-2 ${
-        className ?? ""
-      }`}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      {children}
-    </button>
+    <div className="p-2">
+      <button
+        className={`
+          w-full
+          bg-indigo-300
+          hover:bg-teal-300
+          disabled:opacity-75
+          disabled:pointer-events-none
+          text-center
+          p-4
+          text-xl
+          drop-shadow-md
+          cursor-pointer
+          my-3
+          ${className ?? ""}
+          ${isSelected ? "bg-teal-300" : ""}
+        `}
+        onClick={onClick}
+        disabled={disabled}
+      >
+        {text}
+      </button>
+      {disabled && (
+        <p className="text-center text-gray-600">
+          {votesLength} votes ({percentage} %)
+        </p>
+      )}
+    </div>
   );
 };
