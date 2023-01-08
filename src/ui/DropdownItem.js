06b32666fe3157a71ee32fbd14b060218a963a80
Ruth Temporal <ruthtemp@gmail.com>
2023-01-08 15:44:44 +0100

    modifies ui styles

--- src/ui/DropdownItem.js
+++ src/ui/DropdownItem.js
@@ -1,7 +1,7 @@
-export const DropdownItem = ({ children, onClick }) => {
+export const DropdownItem = ({ children, onClick, className }) => {
   return (
     <li
-      className="text-gray-700 px-4 py-2 text-sm cursor-pointer hover:bg-indigo-50 capitalize border-dotted"
+      className={`text-gray-700 px-4 py-2 text-sm cursor-pointer hover:bg-indigo-50 capitalize ${className}`}
       onClick={onClick}
     >
       {children}
