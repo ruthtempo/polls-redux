7611fb66007e29dbb92c9ae95bf8dc88158adb81
Ruth Temporal <ruthtemp@gmail.com>
2023-01-08 17:15:43 +0100

    adds onclick prop

--- src/ui/NavLink.js
+++ src/ui/NavLink.js
@@ -1,12 +1,13 @@
 import { NavLink as RouterLink } from "react-router-dom";
 
-export const NavLink = ({ to, children }) => {
+export const NavLink = ({ to, children, onClick }) => {
   return (
     <RouterLink
       to={to}
       className={({ isActive }) =>
         `${isActive ? "text-indigo-600" : ""} p-2 tracking-wide`
       }
+      onClick={onClick}
     >
       {children}
     </RouterLink>
